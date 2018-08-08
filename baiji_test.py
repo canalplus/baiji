ec830888e0458b082f9ec10e7bdd7459a6ec4b19
mboulaouche <malika.boulaouche@cpexterne.org>
2018-08-08 17:54:45 +0200

    update test

--- baiji_test.py
+++ baiji_test.py
@@ -2,12 +2,12 @@ from baiji import AliCloudConnect
 
 
 connect = AliCloudConnect(region_id='eu-central-1')
-#print connect.get_accesskey()
+# print connect.get_accesskey()
 # connect.describe_instances()
 # connect.get_role("instance-role")
 # connect.list_users()
 # connect.list_groups()
-connect.describe_vpcs()
+# connect.describe_vpcs()
 
 
 
