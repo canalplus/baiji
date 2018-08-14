d075b7874bd3dadbeeb2a554156014cd2de57beb
mboulaouche <malika.boulaouche@cpexterne.org>
2018-08-14 18:08:02 +0200

    Some coding style and imported logging for basic logs.

--- baiji.py
+++ baiji.py
@@ -1,95 +1,96 @@
+#!/usr/bin/python
+
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
 from aliyunsdkecs.request.v20140526 import StartInstanceRequest
 from aliyunsdkecs.request.v20140526 import StopInstanceRequest
 from aliyunsdkcore.auth.credentials import StsTokenCredential
 
-
 from os import environ
 from os import path
 import requests
 import json
-
+import logging
 
 
 class AliCloudConnect:
+    """
+    """
 
-    #caller will need to pass accessKey, secretKey if stsToken = None / or pass stsToken if it is not none.
-    client = None
-    AccessKeyId = None
-    AccessKeySecretId = None
-    RegionId = None
-    StsToken = None
-
-    #each api has it's own version !
-    version_2014_05_26 = "2014-05-26"
-    version_2015_05_01 = "2015-05-01"
-    version_2015_04_01 = "2015-04-01"
-
-    #endpoint definiton by service
-    ecs_domain = "ecs.eu-central-1.aliyuncs.com"
-    sts_domain = "sts.aliyuncs.com"
-    ram_domain = "ram.aliyuncs.com"
-
-    default_region='eu-central-1'
-
-    protocol = "https"
-
-    def __init__(self, account_id=None , role=None, region_id=default_region):
+    def __init__(self, account_id=None , role=None, region_id="eu-central-1"):
         """
-        Constructor of alicloudConnect
-        We will try to connect with env first, config credentials and finally with instance role
-        User will use the AcsClient to deal with aliyun api
+        Constructor of alicloudConnect.
+
+        We will try to connect with env first, config credentials and finally with instance role.
+        User will use the AcsClient to deal with aliyun api.
+
         :param account_id:
         :param role:
-        :
         """
+
+        # caller will need to pass accessKey, secretKey
+        # if stsToken = None / or pass stsToken if it is not none.
+        self.client = None
+        self.access_key_id = None
+        self.access_secret_key = None
+        self.region_id = region_id
+        self.sts_token = None
+
+        # each api has it's own version !
+        self.version_2014_05_26 = "2014-05-26"
+        self.version_2015_05_01 = "2015-05-01"
+        self.version_2015_04_01 = "2015-04-01"
+
+        # endpoint definiton by service
+        self.ecs_domain = "ecs.eu-central-1.aliyuncs.com"
+        self.sts_domain = "sts.aliyuncs.com"
+        self.ram_domain = "ram.aliyuncs.com"
+
+        self.protocol = "https"
 
         access_key_id = ''
         access_key_secret = ''
 
-
         connect_with_credential = False
         connect_with_role = False
         config_file = path.expanduser('~') + "/.aliyun/config.json"
-        print 'Alicloud connect'
+        logging.info('Connection to Alicloud.')
         if 'ALICLOUD_ACCESS_KEY' in environ:
-            print ' Trying to connect with Env credentials'
+            logging.info('Trying to connect with credentials from environment.')
             access_key_id = environ['ALICLOUD_ACCESS_KEY']
             if 'ALICLOUD_SECRET_KEY' in environ:
                 access_key_secret = environ['ALICLOUD_SECRET_KEY']
                 connect_with_credential = True
-                self.AccessKeyId = access_key_id
-                self.AccessKeySecretId = access_key_secret
-                self.RegionId = region_id
+                self.access_key_id = access_key_id
+                self.access_secret_key = access_key_secret
 
             else:
-                print 'Both access key and secret key are mandatory '
+                logging.info('Both access ALICLOUD_ACCESS_KEY and secret ALICLOUD_SECRET_KEY are mandatory.')
 
         elif path.exists(config_file):
-            print ' Trying to connect with config credentials'
+            logging.info('Trying to connect with config credentials.')
             config = json.load(open(config_file))
             access_key_id = config['profiles'][1]['access_key_id']
             access_key_secret = config['profiles'][1]['access_key_secret']
             region_id = config['profiles'][1]['region_id']
             connect_with_credential = True
-            self.AccessKeyId = access_key_id
-            self.AccessKeySecretId = access_key_secret
+            self.access_key_id = access_key_id
+            self.access_secret_key = access_key_secret
 
         elif account_id is not None and role is not None:
-            print 'Trying to connect with instance role'
+            logging.info('Trying to connect with instance role.')
             response = requests.get('http://100.100.100.200/latest/meta-data/ram/security-credentials/%s' % role, timeout=5).json()
 
             sts_access_key = response['AccessKeyId']
             sts_secret_key = response['AccessKeySecret']
             sts_token_session = response['SecurityToken']
 
-            self.AccessKeyId = sts_access_key
-            self.AccessKeySecretId = sts_secret_key
+            self.access_key_id = sts_access_key
+            self.access_secret_key = sts_secret_key
             connect_with_role = True
 
         else:
-            print 'no valid connection type provided'
+            logging.error('No valid connection type provided.')
 
         try:
             if connect_with_credential:
@@ -99,28 +100,31 @@ class AliCloudConnect:
                 sts_token_credential = StsTokenCredential(sts_access_key,
                                                           sts_secret_key,
                                                           sts_token_session)
-                self.StsToken = sts_token_credential
+                self.sts_token = sts_token_credential
                 self.client = AcsClient(region_id='cn-hangzhou', credential=sts_token_credential)
 
         except Exception as e:
-            print e
+            logging.error(e)
 
     def pretty_print(self, toprint):
         print json.dumps(json.loads(toprint), indent=4, sort_keys=True)
 
     def get_accesskey(self):
-        return self.AccessKeyId
+        return self.access_key_id
 
     def get_secretkey(self):
-        return self.AccessKeySecretId
+        return self.access_secret_key
 
     def get_ststoken(self):
-        return self.StsToken
+        return self.sts_token
 
     def assume_role(self, role_arn, session_name):
         '''
-        Assume a role : this function will try to assume a role. and will set StsToken of the constructor if OK
+        assume_role
+
+        This function will try to assume a role. and will set StsToken of the constructor if OK.
         User will need to give this token when calling aliyun api.
+
         Example of use
             ali_connect = AliCloudConnect()
             ali_connect.assume_role("acs:ram::<<account_ID>>:role/ecsadmin", "test")
@@ -132,7 +136,7 @@ class AliCloudConnect:
         :return:
         '''
 
-        print 'Going to assume role: ' + role_arn + ' sessionName: ' + session_name
+        logging.info("Going to assume role:  {} sessionName: {}".format(role_arn, session_name))
         request = CommonRequest()
         request.set_domain(self.sts_domain)
         request.set_version(self.version_2015_04_01)
@@ -145,24 +149,26 @@ class AliCloudConnect:
             sts_access_key = response['Credentials']['AccessKeyId']
             sts_secret_key = response['Credentials']['AccessKeySecret']
             sts_token_session = response['Credentials']['SecurityToken']
-            self.StsToken = StsTokenCredential(sts_access_key,
-                                                          sts_secret_key,
-                                                          sts_token_session)
+            self.sts_token = StsTokenCredential(
+                sts_access_key, sts_secret_key, sts_token_session
+            )
 
             return response
         except Exception as e:
-            print e
+            logging.error(e)
 
-###############################
-############ECS################
-###############################
+    ###############################
+    ############ECS################
+    ###############################
 
     def describe_instances(self):
         """
-        Describe instance
+        Describe instances.
+
         Example of call :
             ali_connect = AliCloudConnect()
             ali_connect.describe_instances()
+
         :return:
         """
         print 'Going to describe all instances'
@@ -181,10 +187,12 @@ class AliCloudConnect:
 
     def describe_disks(self):
         """
-        Describe instance
+        Describe disks.
+
         Example of call :
             ali_connect = AliCloudConnect()
             ali_connect.describe_instances()
+
         :return:
         """
         print 'Going to describe all disks'
@@ -204,7 +212,8 @@ class AliCloudConnect:
 
     def stop_instances(self, instanceid, sts_token = None):
         """
-        Stop Alicloud instance
+        Stop Alicloud instances.
+
         if Ram user have the persmission to do this action we can simply call:
             ali_connect = AliCloudConnect()
             ali_connect.stop_instances('azertyuiop')
@@ -217,7 +226,7 @@ class AliCloudConnect:
         :param sts_token:
         :return:
         """
-        print 'Going to stop instance ' + instanceid
+        logging.info("Going to stop instance: {}".format(instanceid))
 
         request = StopInstanceRequest.StopInstanceRequest()
         request.set_InstanceId(instanceid)
@@ -228,17 +237,18 @@ class AliCloudConnect:
                 response = acs_client.do_action_with_exception(request)
                 self.pretty_print(response)
             except Exception as e:
-                print e
+                logging.error(e)
         else:
             try:
                 response = self.client.do_action_with_exception(request)
                 self.pretty_print(response)
             except Exception as e:
-                print e
+                logging.error(e)
 
     def start_instances(self, instanceid, sts_token=None):
         """
-        Start Alicloud instance
+        Start Alicloud instances.
+
         if Ram user have the persmission to do this action we can simply call:
             ali_connect = AliCloudConnect()
             ali_connect.start_instances('azertyuiop')
@@ -251,7 +261,7 @@ class AliCloudConnect:
         :param sts_token:
         :return:
         """
-        print 'Going to start instance ' + instanceid
+        logging.info("Going to start instance: {}".format(instanceid))
 
         request = StartInstanceRequest.StartInstanceRequest()
         request.set_InstanceId(instanceid)
@@ -262,27 +272,29 @@ class AliCloudConnect:
                 response = acs_client.do_action_with_exception(request)
                 self.pretty_print(response)
             except Exception as e:
-                print e
+                logging.error(e)
         else:
             try:
                 response = self.client.do_action_with_exception(request)
                 self.pretty_print(response)
             except Exception as e:
-                print e
+                logging.error(e)
 
-###############################
-############VPC################
-###############################
+    ###############################
+    ############VPC################
+    ###############################
 
     def describe_vpcs(self):
         """
-        Describe vpcs
+        Describe vpcs.
+
         Example of call :
             ali_connect = AliCloudConnect()
             ali_connect.describe_vpcs()
+
         :return:
         """
-        print 'Going to describe all vpcs'
+        logging.info('Going to describe all vpcs.')
 
         request = CommonRequest()
         request.set_domain(self.ecs_domain)
@@ -293,19 +305,21 @@ class AliCloudConnect:
             response = self.client.do_action_with_exception(request)
             self.pretty_print(response)
         except Exception as e:
-            print e
+            logging.error(e)
 
 
 
     def describe_vswitchs(self):
         """
-        Describe vswitches
+        Describe vswitches.
+
         Example of call :
             ali_connect = AliCloudConnect()
             ali_connect.describe_vswitches()
+
         :return:
         """
-        print 'Going to describe all vswitches'
+        logging.info('Going to describe all vswitches.')
 
         request = CommonRequest()
         request.set_domain(self.ecs_domain)
@@ -316,22 +330,25 @@ class AliCloudConnect:
             response = self.client.do_action_with_exception(request)
             self.pretty_print(response)
         except Exception as e:
-            print e
+            logging.error(e)
 
-###############################
-############RAM################
-###############################
+    ###############################
+    ############RAM################
+    ###############################
 
 
     def create_role(self, role_name, policy_document):
         """
+        create_role
+
+        Creates a RAM role.
 
         :param self:
         :param role_name:
         :param policy_document:
         :return:
         """
-        print 'Going to create role: ' + role_name
+        logging.info("Going to create role: {}".format(role_name))
         request = CommonRequest()
         request.set_domain(self.ram_domain)
         request.set_version(self.version_2015_05_01)
@@ -341,20 +358,23 @@ class AliCloudConnect:
         request.set_action_name('CreateRole')
         try:
             response = self.client.do_action_with_exception(request)
-            print response
+            loggging.info(response)
         except Exception as e:
-            print e
+            logging.error(e)
 
 
 
     def get_role(self, role_name):
         """
+        get_role
+
+        Gets data of a RAM role.
 
         :param alicloud_client:
         :param role_name:
         :return:
         """
-        print 'Going to get role ' + role_name
+        logging.info("Going to get role {}".format(role_name))
         request = CommonRequest()
         request.set_domain(self.ram_domain)
         request.set_version(self.version_2015_05_01)
@@ -365,16 +385,19 @@ class AliCloudConnect:
             response = self.client.do_action_with_exception(request)
             self.pretty_print(response)
         except Exception as e:
-            print e
+            logging.error(e)
 
 
     def list_users(self):
         """
+        list_users
+
+        Lists RAM users.
 
         :param alicloud_client:
         :return:
         """
-        print 'Going to list users'
+        logging.info('Going to list users.')
         request = CommonRequest()
         request.set_domain(self.ram_domain)
         request.set_version(self.version_2015_05_01)
@@ -384,16 +407,19 @@ class AliCloudConnect:
             response = self.client.do_action_with_exception(request)
             self.pretty_print(response)
         except Exception as e:
-            print e
+            logging.error(e)
 
 
     def list_groups(self):
         """
+        list_groups
+
+        Lists RAM groups.
 
         :param alicloud_client:
         :return:
         """
-        print 'Going to list groups'
+        logging.info('Going to list groups.')
         request = CommonRequest()
         request.set_domain(self.ram_domain)
         request.set_version(self.version_2015_05_01)
@@ -403,24 +429,4 @@ class AliCloudConnect:
             response = self.client.do_action_with_exception(request)
             self.pretty_print(response)
         except Exception as e:
-            print e
-
-    def list_groups(self):
-        """
-
-        :param alicloud_client:
-        :return:
-        """
-        print 'Going to list Policies'
-        request = CommonRequest()
-        request.set_domain(self.ram_domain)
-        request.set_version(self.version_2015_05_01)
-        request.set_protocol_type(self.protocol)
-        request.set_action_name('ListPolicies')
-        try:
-            response = self.client.do_action_with_exception(request)
-            self.pretty_print(response)
-        except Exception as e:
-            print e
-
-
+            logging.error(e)
