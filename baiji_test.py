d2354ea32de86f913a920ae2e7f5e88c23d866ef
Benoit Petit <bpetit@b0rk.in>
2018-08-20 16:57:02 +0200

    Implented basic tests on security groups.

--- baiji_test.py
+++ baiji_test.py
@@ -1,19 +1,14 @@
 from baiji import AliCloudConnect
+import sys
+import traceback
 
-
-connect = AliCloudConnect(region_id='eu-central-1')
-# print connect.get_accesskey()
-# connect.describe_instances()
-# connect.get_role("instance-role")
-# connect.list_users()
-# connect.list_groups()
-# connect.describe_vpcs()
-
-
-
-
-
-
-
-
-
+try:
+    connect = AliCloudConnect(region_id='eu-central-1')
+    clt = connect.client('ecs')
+    clt.security_groups.all()
+    print("Resources successfully fetched.")
+    sys.exit(0)
+except Exception as e:
+    print(e)
+    print(traceback.format_exc())
+    sys.exit(1)
