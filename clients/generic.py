6af533b2aeccf622fdcfe33ebbac59236c93c51c
mboulaouche <malika.boulaouche@cpexterne.org>
2018-11-19 16:32:37 +0100

    add protocol parameter to class client and modifications of the domain parameter to use also RAM

--- clients/generic.py
+++ clients/generic.py
@@ -8,35 +8,35 @@ import sys
 
 class Client(object):
 
-    def __init__(self, client, base_domain='aliyuncs.com', domain_prefix=None, version=None, region='eu-central-1'):
+    def __init__(self, client, base_domain='aliyuncs.com', domain_prefix=None, version=None, protocol=None, region='eu-central-1'):
         super(Client, self).__init__()
         self.__client = client
-        self.__base_domain = None
         self.__base_domain = base_domain
-        self.__domain_prefix = None
-        self.__version = None
-        if domain_prefix:
-            self.__domain_prefix = domain_prefix
-        if version:
-            self.__version = version
+        self.__domain_prefix = domain_prefix
+        self.__version = version
         self.__region = region
+        self.__protocol = protocol
         # you have to define the full domain in the child classes
         # it should __domain_prefix + [ __region +] __base_domain
         self.__domain = None
 
 class ResourceCollection(object):
 
-    def __init__(self, client, domain, version):
+    def __init__(self, client, domain, version, protocol):
         super(ResourceCollection, self).__init__()
         self.__client = client
-        self.__domain = domain
-        self.__version = version
+        self.domain = domain
+        self.version = version
+        self.protocol = protocol
 
     def request(self, action, params, resource_class):
         request = CommonRequest()
-        request.set_domain(self.__domain)
-        request.set_version(self.__version)
+        request.set_domain(self.domain)
+        request.set_version(self.version)
         request.set_action_name(action)
+        request.set_accept_format('json')
+        if self.protocol:
+            request.set_protocol_type (self.protocol)
         if 'api_params' in params and params['api_params']:
             request.set_query_params(params['api_params'])
         try:
