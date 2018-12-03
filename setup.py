5c96b8a0ccd9f90bfe07e3a7f293deb3308bf4f9
Malika Boulaouche <malika.boulaouche@cpexterne.org>
2018-12-03 10:05:56 +0100

    remove author and author_email options from setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.4',  # Required
+    version='1.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -79,11 +79,11 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Boulaouche Malika',  # Optional
+    #author='',  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='s.boulaouche@outlook.com',  # Optional
+    #author_email='',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
